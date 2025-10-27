836960a3470bd3b95879765378e224f7590a6c8c
john <elitejhn5@gmail.com>
2025-10-27 17:54:24 +0100

    additional designs for player page

--- lib/screens/player.dart
+++ lib/screens/player.dart
@@ -54,79 +54,230 @@ class PlayerScreen extends ConsumerWidget {
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 30.0),
                   child: Card(
+                    clipBehavior: Clip.hardEdge,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadiusGeometry.circular(10),
+                    ),
                     elevation: 8.0,
-                    child: Center(
-                      child: Column(
-                        children: [
-                          Expanded(
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                left: 8,
-                                right: 8,
-                                top: 8,
+                    child: Container(
+                      decoration: ref.watch(isDarkmodeProvider)
+                          ? const BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  Color.fromARGB(255, 58, 58, 58),
+                                  Color.fromARGB(255, 0, 0, 0),
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
                               ),
-                              child: Container(
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10),
-                                  image: DecorationImage(
-                                    image: AssetImage('assets/ocean.jpg'),
-                                    fit: BoxFit.cover,
+                            )
+                          : null,
+                      child: Center(
+                        child: Column(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 8,
+                                  right: 8,
+                                  top: 8,
+                                ),
+                                child: Container(
+                                  width: double.infinity,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    image: DecorationImage(
+                                      image: AssetImage('assets/ocean.jpg'),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 10,
-                              vertical: 20,
-                            ),
-                            child: Row(
-                              children: [
-                                Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Flexible(
-                                      child: Text(
-                                        'In My Heart',
-                                        style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black,
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                                vertical: 20,
+                              ),
+                              child: Row(
+                                children: [
+                                  Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Flexible(
+                                        child: Text(
+                                          'In My Heart',
+                                          style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: ref.watch(isDarkmodeProvider)
+                                                ? Colors.white.withValues(
+                                                    alpha: 0.3,
+                                                  )
+                                                : Colors.black,
+                                          ),
                                         ),
                                       ),
-                                    ),
-                                    Flexible(
-                                      child: Text(
-                                        'Acid',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.grey,
+                                      Flexible(
+                                        child: Text(
+                                          'Acid',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.grey,
+                                          ),
                                         ),
                                       ),
-                                    ),
 
-                                    // const Expanded(
-                                    //   child: SizedBox(
-                                    //     width: double.infinity,
-                                    //   ),
-                                    // ),
-                                  ],
-                                ),
-                                const Expanded(child: SizedBox(width: double.infinity,)),
-                                IconButton(
-                                  onPressed: () {},
-                                  icon: const Icon(Icons.favorite),
-                                ),
-                              ],
+                                      // const Expanded(
+                                      //   child: SizedBox(
+                                      //     width: double.infinity,
+                                      //   ),
+                                      // ),
+                                    ],
+                                  ),
+                                  const Expanded(
+                                    child: SizedBox(
+                                      width: double.infinity,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: const Icon(
+                                      Icons.favorite,
+                                      color: Colors.pinkAccent,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
                     ),
                   ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      '0:00',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.shuffle,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.repeat,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      '3:00',
+                      style: TextStyle(fontSize: 14, color: Colors.grey),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15.0),
+                child: Text('seek goes here'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(
+                  left: 20.0,
+                  right: 20.0,
+                  bottom: 35,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Card(
+                      clipBehavior: Clip.hardEdge,
+                      elevation: 10,
+                      child: Container(
+                        decoration: ref.watch(isDarkmodeProvider)
+                            ? const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(255, 58, 58, 58),
+                                    Colors.black,
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              )
+                            : null,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.skip_previous, color: ref.watch(isDarkmodeProvider) ? Colors.white : Colors.black,),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        clipBehavior: Clip.hardEdge,
+                        elevation: 10,
+                        child: Container(
+                          decoration: ref.watch(isDarkmodeProvider)
+                              ? const BoxDecoration(
+                                  gradient: LinearGradient(
+                                    colors: [
+                                      Color.fromARGB(255, 58, 58, 58),
+                                      Colors.black,
+                                    ],
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                  ),
+                                )
+                              : null,
+                          width: double.infinity,
+                          child: Center(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20),
+                              child: Icon(Icons.play_arrow,color: ref.watch(isDarkmodeProvider) ? Colors.white : Colors.black,),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      clipBehavior: Clip.hardEdge,
+                      elevation: 10,
+                      child: Container(
+                        decoration: ref.watch(isDarkmodeProvider)
+                            ? const BoxDecoration(
+                                gradient: LinearGradient(
+                                  colors: [
+                                    Color.fromARGB(255, 58, 58, 58),
+                                    Colors.black,
+                                  ],
+                                  begin: Alignment.topLeft,
+                                  end: Alignment.bottomRight,
+                                ),
+                              )
+                            : null,
+                        child: Center(
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Icon(Icons.skip_next, color: ref.watch(isDarkmodeProvider) ? Colors.white : Colors.black,),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
